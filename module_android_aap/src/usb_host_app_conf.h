c9dee0413da825cb7a08cf76802eb0f4930b9906
Paul Hampson <paul@xmos.com>
2011-11-14 13:11:27 +0000

    added reconnect pause, cleaned up some comments

--- module_android_aap/src/usb_host_app_conf.h
+++ module_android_aap/src/usb_host_app_conf.h
@@ -5,19 +5,22 @@
 #define NUM_CTRL_CHANENDS 1
 
 /* set the configuration to start with - useful if we know which configuration we want */
-#define START_CONFIG_NUM 1
+#define START_CONFIG_NUM 0
 
 /* maximum number of timed EP's we have (e.g. Isoc / Interrupt EPs) */
 #define TIMED_EP_COUNT 0
 
-/* Initial get descriptor length to try, will retry if length is longer */
+/* Initial get descriptor length to try, will retry if actual length is longer */
 #define INITIAL_GET_DEV_DESC_LEN         18
 
-/* Initial get descriptor length to try, will retry if length is longer */
+/* EP0 Pipe width - for FS this should only ever be 64 bytes */
 #define DEFAULT_EP0_PIPE_WIDTH         64
 
 /* Initial get configuration descriptor length to try, will retry if length is longer */
 #define INITIAL_GET_CONF_DESC_LEN    255
+
+/* Define the amount of time between connections in mS (for 0ms comment this define out) */
+#define RECONNECT_PAUSE     10
 
 /* enable hook for VID/PID match failure */
 #define USBHOST_VID_PID_HOOK_ON
